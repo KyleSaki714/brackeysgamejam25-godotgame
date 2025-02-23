a9112f5312b7f8a32f6e37258e4facdb3327ec1d
KyleSaki714 <kyle13san@Gmail.com>
2025-02-23 00:03:01 -0800

    add more assets comic panels! finish end screen!

--- Screens/EndScreen.gd
+++ Screens/EndScreen.gd
@@ -1,4 +1,15 @@
 extends Control
 
+var currentpage = 0
+
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Screens/MainMenu.tscn")
+	match currentpage:
+		0:
+			$Panel2.show()
+			$Panel2/AnimationPlayer.play("enter")
+		1:
+			$Panel2/AnimationPlayer.play("flippage")
+			$Button.text = "Bye"
+		2:
+			get_tree().change_scene_to_file("res://Screens/MainMenu.tscn")
+	currentpage += 1
